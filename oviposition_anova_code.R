1f89f564e9a6cdf158f441590fbad5b8fa820292
myersan7 <landislab@cns.msu.edu>
2017-01-03 10:17:13 -0500

    pared down code significantly. removed individual plots for deployments 1 and 2. implemented changes made to deployment 3 anova to the first anova that included deployments 1, 2, and 3

--- oviposition_anova_code.R
+++ oviposition_anova_code.R
@@ -1,58 +1,61 @@
 
 oviposition<-read.csv(file="oviposition.csv", header=TRUE) #read in oviposition file
-
 oviposition<-na.omit(oviposition) #get rid of na's. There were several incidents when we were unable to count eggs (broken plants, plants were covered by exclosures, etc)
+
 
 #install and use the ddply function to average across subsamples (individual plants within plant patches)
 library(plyr)
-oviposition.avg <-ddply(oviposition, .(treatment, date, time, block, deployment), summarize, monarch_eggs.mean=mean(monarch_eggs))
+oviposition.avg <-ddply(oviposition, .(treatment, date, time, block, deployment), summarize, 
+                                    monarch_eggs.mean=mean(monarch_eggs),
+                                    monarch_eggs.sum=sum(monarch_eggs),
+                                    nplants=length(monarch_eggs))
 
-#make block, date, time, deployment into a factor
+#make block, date, time, deployment into factors
 oviposition.avg$block <- as.factor(oviposition.avg$block)
 oviposition.avg$date <- as.factor(oviposition.avg$date)
 oviposition.avg$time <- as.factor(oviposition.avg$time)
 oviposition.avg$deployment <- as.factor(oviposition.avg$deployment)
 
 #do the anova using glm function
-result <- glm(monarch_eggs.mean ~ block + treatment, data=oviposition.avg)
+result <- glm(monarch_eggs.sum ~ block + treatment, offset=nplants, data=oviposition.avg)
+summary(result)
 aov(result)
 summary(aov(result))
 TukeyHSD(aov(result))
 
-#make the mean plot
-library(gplots)
-plotmeans(monarch_eggs.mean~treatment,xlab="Habitat Treatment",
-          ylab="Mean Eggs/Plant Obs", main="Mean Plot Across Entire Field Season\nwith 95% CI", data=oviposition.avg)
-
-#make date and time variables factors
-as.factor(oviposition.avg$date)
-as.factor(oviposition.avg$time)
-
-#rerun anova with date and time as covariates
-result_covariates <- glm(monarch_eggs.mean ~ block + treatment + date + time, data=oviposition.avg)
+#rerun anova with appropriate data as factors, with sum of eggs as response variable
+result_covariates <- glm(monarch_eggs.sum ~ block + treatment, offset=nplants, data=oviposition.avg)
+summary(result_covariates)
 aov(result_covariates)
 summary(aov(result_covariates))
 TukeyHSD(aov(result_covariates))
 #rerun this with poisson distribution
-result_covariates.poisson <- glm(monarch_eggs.mean ~ block + treatment + date + time, data=oviposition.avg, family = poisson())
+result_covariates.poisson <- glm(monarch_eggs.sum ~ block + treatment, offset=nplants, data=oviposition.avg, family = "poisson")
+summary(result_covariates.poisson)
 aov(result_covariates.poisson)
 summary(aov(result_covariates.poisson))
 TukeyHSD(aov(result_covariates.poisson))
+#residual deviance is high. Switch to a negative binomial model
+library(pscl)
+result_covariates.nb <- glm.nb(monarch_eggs.sum ~ block + treatment +offset(nplants), data=oviposition.avg)
+summary(result_covariates.nb)
+aov(result_covariates.nb)
+summary(aov(result_covariates.nb))
+#need a holm-adjusted t-test here because Tukey doesn't work with NB models
 
 #need to use ddply to compute summary stats for plotting
-
+#this one includes deployment number for faceting ggplot later
 oviposition.summary<-ddply(oviposition.avg, .(treatment, deployment), summarize,
                            N=length(monarch_eggs.mean),
                            mean=mean(monarch_eggs.mean),
                            sd   = sd(monarch_eggs.mean),
                            se   = sd / sqrt(N) )
+#this one does not include deployment number
 oviposition.summary.overall<-ddply(oviposition.avg, .(treatment), summarize,
-                           N=length(monarch_eggs.mean),
-                           mean=mean(monarch_eggs.mean),
-                           sd   = sd(monarch_eggs.mean),
-                           se   = sd / sqrt(N) )
-
-head(oviposition.summary)
+                                   N=length(monarch_eggs.mean),
+                                   mean=mean(monarch_eggs.mean),
+                                   sd   = sd(monarch_eggs.mean),
+                                   se   = sd / sqrt(N) )
 
 #make a bar plot with ggplot
 library(ggplot2)
@@ -67,28 +70,11 @@ ggplot(oviposition.summary.overall, aes(x=treatment, y=mean, fill=treatment)) +
   ggtitle("Monarch Butterfly oviposition by Habitat with SE Bars") +
   theme(panel.background = element_blank())
 
-
-
-
-
-
-
-
+oviposition.deployment3.avg<- subset(oviposition.avg, deployment==3)
 
 #Doug asked me to redo the ANOVA with only the third egg check
 # this is where the most important separations will be observed- sample sizes were very low in first two
-oviposition.deployment3<-subset(oviposition, deployment==3)
-
-#install and use the ddply function to average across subsamples (individual plants within plant patches)
-oviposition.deployment3.avg <-ddply(oviposition.deployment3, .(treatment, date, time, block), summarize, 
-                                    monarch_eggs.mean=mean(monarch_eggs),
-                                    monarch_eggs.sum=sum(monarch_eggs),
-                                    nplants=length(monarch_eggs))
-
-#make block, date, and time into a factor
-oviposition.deployment3.avg$block <- as.factor(oviposition.deployment3.avg$block)
-oviposition.deployment3.avg$date <- as.factor(oviposition.deployment3.avg$date)
-oviposition.deployment3.avg$time <- as.factor(oviposition.deployment3.avg$time)
+oviposition.deployment3.avg<- subset(oviposition.avg, deployment==3)
 
 #do the anova using glm function
 result.deployment3 <- glm(monarch_eggs.sum ~ block + treatment, offset=nplants, data=oviposition.deployment3.avg)
@@ -96,16 +82,6 @@ summary(result.deployment3)
 aov(result.deployment3)
 summary(aov(result.deployment3))
 TukeyHSD(aov(result.deployment3))
-
-#make the mean plot
-library(gplots)
-plotmeans(monarch_eggs.mean~treatment,xlab="Habitat Treatment",
-          ylab="Mean Eggs/Plant Obs", main="Mean Plot Across Entire Field Season\nwith 95% CI", data=oviposition.deployment3.avg)
-
-#make date and time variables factors
-oviposition.deployment3.avg$date<-as.factor(oviposition.deployment3.avg$date)
-oviposition.deployment3.avg$time<-as.factor(oviposition.deployment3.avg$time)
-oviposition.deployment3.avg$block<-as.factor(oviposition.deployment3.avg$block)
 
 #rerun anova with appropriate data as factors, with sum of eggs as response variable
 result_covariates.deployment3 <- glm(monarch_eggs.sum ~ block + treatment, offset=nplants, data=oviposition.deployment3.avg)
@@ -130,7 +106,6 @@ summary(aov(result_covariates.deployment3.nb))
 
 TukeyHSD(aov(result_covariates.deployment3.nb))
 
-
 #need to use ddply to compute summary stats for plotting
 
 oviposition.deployment3.summary<-ddply(oviposition.deployment3.avg, .(treatment), summarize,
@@ -152,73 +127,6 @@ ggplot(oviposition.deployment3.summary, aes(x=treatment, y=mean, fill=treatment)
   ylab("monarch eggs / plant obervation" )+
   ggtitle("Monarch Butterfly oviposition.deployment3 by Habitat with SE Bars") +
   theme(panel.background = element_blank())
-
-
-
-
-
-#making individual bar plots for deployments 1 & 2
-#trying to facet it...
-cols <- c("gold2", "firebrick1", "yellowgreen", "mediumpurple", "dodgerblue2" )
-ggplot(oviposition.summary, aes(x=treatment, y=mean, fill=treatment)) + 
-  geom_bar(position=position_dodge(), stat="identity", size=.3, fill=) +
-  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=.2, position=position_dodge(.9)) +
-  scale_color_manual(values=cols)+
-  ylab("monarch eggs / plant obervation" )+
-  ggtitle("Monarch Butterfly oviposition by Habitat with SE Bars") +
-  theme(panel.background = element_blank())+
-  facet_wrap(~deployment)
-
-
-#doing it the long way...
-oviposition.deployment1<-subset(oviposition, deployment==1)
-oviposition.deployment2<-subset(oviposition, deployment==2)
-
-#install and use the ddply function to average across subsamples (individual plants within plant patches)
-oviposition.deployment1.avg <-ddply(oviposition.deployment1, .(treatment, date, time, block), summarize, monarch_eggs.mean=mean(monarch_eggs))
-oviposition.deployment2.avg <-ddply(oviposition.deployment2, .(treatment, date, time, block), summarize, monarch_eggs.mean=mean(monarch_eggs))
-
-#make block, date, and time into a factor
-oviposition.deployment1.avg$block <- as.factor(oviposition.deployment1.avg$block)
-oviposition.deployment1.avg$date <- as.factor(oviposition.deployment1.avg$date)
-oviposition.deployment1.avg$time <- as.factor(oviposition.deployment1.avg$time)
-
-oviposition.deployment2.avg$block <- as.factor(oviposition.deployment2.avg$block)
-oviposition.deployment2.avg$date <- as.factor(oviposition.deployment2.avg$date)
-oviposition.deployment2.avg$time <- as.factor(oviposition.deployment2.avg$time)
-
-#need to use ddply to compute summary stats for plotting
-
-oviposition.deployment1.summary<-ddply(oviposition.deployment1.avg, .(treatment), summarize,
-                                       N=length(monarch_eggs.mean),
-                                       mean=mean(monarch_eggs.mean),
-                                       sd   = sd(monarch_eggs.mean),
-                                       se   = sd / sqrt(N) )
-
-oviposition.deployment2.summary<-ddply(oviposition.deployment2.avg, .(treatment), summarize,
-                                       N=length(monarch_eggs.mean),
-                                       mean=mean(monarch_eggs.mean),
-                                       sd   = sd(monarch_eggs.mean),
-                                       se   = sd / sqrt(N) )
-
-#make a bar plot with ggplot
-#cols is my personalized colour palette.
-cols <- c("gold2", "firebrick1", "yellowgreen", "mediumpurple", "dodgerblue2" )
-ggplot(oviposition.deployment1.summary, aes(x=treatment, y=mean, fill=treatment)) + 
-  geom_bar(position=position_dodge(), stat="identity", size=.3, fill=cols) +
-  scale_color_manual(values=cols)+
-  ylab("monarch eggs / plant obervation" )+
-  ggtitle("Monarch Butterfly oviposition.deployment1 by Habitat with SE Bars") +
-  theme(panel.background = element_blank())
-
-ggplot(oviposition.deployment2.summary, aes(x=treatment, y=mean, fill=treatment)) + 
-  geom_bar(position=position_dodge(), stat="identity", size=.3, fill=cols) +
-  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=.2, position=position_dodge(.9)) +
-  scale_color_manual(values=cols)+
-  ylab("monarch eggs / plant obervation" )+
-  ggtitle("Monarch Butterfly oviposition.deployment2 by Habitat with SE Bars") +
-  theme(panel.background = element_blank())
-
 
 #faceted bar chart!
 
