cfba0459aa713366bffac505510791229cdb7e57
myersan7 <landislab@cns.msu.edu>
2017-03-10 13:39:24 -0500

    trying to do NMDS

--- predator_NMDS.R
+++ predator_NMDS.R
@@ -5,14 +5,13 @@ pred_data1<-read.csv(file="predator_surveys_deployment1.csv", header=TRUE)
 
 #create a column that combines block and treatment
 pred_data1$block.treatment = paste(pred_data1$block, pred_data1$treatment, sep=".")
+block.treatment<-pred_data1[,24]
 
-#make it into a matrix without the extra stuff
-pred_data1_matrix<-as.matrix(pred_data1[,5:24])
+#make it into a numeric matrix without the extra stuff
+pred_data1_matrix<-data.matrix(pred_data1[,5:23])
 
-#moving the block.treatment column to the front of the matrix in probably a very inefficient way
-block.treatment<-pred_data1_matrix[,20]
-pred_data1_matrix<-pred_data1_matrix[,1:19]
-pred_data1_matrix<-cbind(block.treatment,pred_data1_matrix)
 
-#try NMDS
+library(vegan)
+#try NMDS, not sure if it worked
 metaMDS(pred_data1_matrix, k=2, distance="bray")
+
